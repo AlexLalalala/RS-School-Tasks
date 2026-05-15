a5eda75c92a31a5438d92031df6968c6e58cb9e1
Alexkyky <alexkyku@gmail.com>
2026-05-15 19:56:40 +0400

    refactor: convert GameCard component from class to functional component

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import { buildMetacriticURL } from '../utils';
 
 interface GameCardProps {
@@ -9,35 +8,34 @@ interface GameCardProps {
   metacriticLink: string;
 }
 
-class GameCard extends Component<GameCardProps, object> {
-  render() {
-    return (
-      <>
-        <div className="card h-100" style={{ width: '18rem' }}>
-          <img
-            src={this.props.thumb}
-            className="card-img-top"
-            alt={`Thumbnail of ${this.props.title}`}
-          />
-          <div className="card-body">
-            <h5 className="card-title">{this.props.title}</h5>
-            <p className="card-text">
-              <span className="text-body-secondary text-decoration-line-through">
-                {this.props.normalPrice}
-              </span>{' '}
-              <span className="fw-bold fs-5">{this.props.salePrice}</span>
-            </p>
-            <a
-              href={buildMetacriticURL(this.props.metacriticLink)}
-              className="btn btn-primary"
-            >
-              See Reviews
-            </a>
-          </div>
+function GameCard({title, normalPrice, salePrice, thumb, metacriticLink}:GameCardProps){
+  return (
+    <>
+      <div className="card h-100" style={{ width: '18rem' }}>
+        <img
+          src={thumb}
+          className="card-img-top"
+          alt={`Thumbnail of ${title}`}
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">
+            <span className="text-body-secondary text-decoration-line-through">
+              {normalPrice}
+            </span>{' '}
+            <span className="fw-bold fs-5">{salePrice}</span>
+          </p>
+          <a
+            href={buildMetacriticURL(metacriticLink)}
+            className="btn btn-primary"
+          >
+            See Reviews
+          </a>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
+
 
 export default GameCard;

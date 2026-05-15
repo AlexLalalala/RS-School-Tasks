f8368891ba14cfd022fec38a811b6dfe347f4b0a
Alexkyky <alexkyku@gmail.com>
2026-05-15 18:58:02 +0400

    chore: update package.json and package-lock.json for gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'RS-School-Tasks',
   plugins: [react()],
   test: {
     environment: 'jsdom',
